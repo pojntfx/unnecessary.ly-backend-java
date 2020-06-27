6565aba7e09b08a913379ab09142272ca132adb1
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-27 12:55:28 +0200

    feat: Start adding ORM

--- src/main/java/ly/unnecessary/backend/Application.java
+++ src/main/java/ly/unnecessary/backend/Application.java
@@ -8,23 +8,29 @@ import ly.unnecessary.backend.rooms.RoomsServiceImpl;
 
 public class Application {
     public static void main(String[] args) throws InterruptedException, IOException {
-        // Parse port flag
-        var portFlag = System.getenv("PORT");
+        // Parse flags
+        var lportFlag = System.getenv("LPORT");
+        var dbusrFlag = System.getenv("DBUSR");
+        var dbpassFlag = System.getenv("DBPASS");
+        var dbhostFlag = System.getenv("DBHOST");
+        var dbportFlag = System.getenv("DBPORT");
+        var dbnameFlag = System.getenv("DBNAME");
 
-        final int port;
-        if (portFlag == null) {
-            port = 1999;
-        } else {
-            port = Integer.valueOf(portFlag);
-        }
+        // Use default values if flags are not set
+        final var lport = (lportFlag == null) ? 1999 : Integer.valueOf(lportFlag);
+        final var dbusr = (dbusrFlag == null) ? "pojntfx" : dbusrFlag;
+        final var dbpass = (dbpassFlag == null) ? "pojntfx" : dbpassFlag;
+        final var dbhost = (dbhostFlag == null) ? "localhost" : dbhostFlag;
+        final var dbport = (dbportFlag == null) ? 5432 : Integer.valueOf(dbportFlag);
+        final var dbname = (dbnameFlag == null) ? "pojntfx" : dbnameFlag;
 
-        System.out.printf("Starting server on port %d\n", port);
+        System.out.printf("Starting server on port %d\n", lport);
 
         // Create services
         var roomsCore = new RoomsCoreImpl();
         var roomsService = new RoomsServiceImpl(roomsCore);
 
         // Serve services
-        ServerBuilder.forPort(port).addService(roomsService).build().start().awaitTermination();
+        ServerBuilder.forPort(lport).addService(roomsService).build().start().awaitTermination();
     }
 }
