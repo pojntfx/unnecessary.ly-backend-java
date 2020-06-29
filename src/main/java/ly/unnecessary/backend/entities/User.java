71366d9272fc5b3de3bc0849177fcd15b15f9083
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-06-30 01:49:51 +0200

    feat: Add basic user model

---
+++ src/main/java/ly/unnecessary/backend/entities/User.java
@@ -0,0 +1,54 @@
+package ly.unnecessary.backend.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String displayName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/ly/unnecessary/backend/services/RoomsService.java
+++
@@ -1,7 +0,0 @@
-package ly.unnecessary.backend.services;
-
-import io.grpc.BindableService;
-
-public interface RoomsService extends BindableService {
-
-}
