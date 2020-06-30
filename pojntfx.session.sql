3448c0e7a51d939b6d5c9cc66afee34de57e7762
Felix Pojtinger <felix@pojtinger.com>
2020-06-30 20:46:56 +0200

    feat: Implement mailer

--- pojntfx.session.sql
+++
@@ -1,3 +0,0 @@
-UPDATE user_sign_up_request
-SET confirmed = false
-WHERE id = 1;
--- src/main/java/ly/unnecessary/backend/utilities/UserEmailer.java
+++ src/main/java/ly/unnecessary/backend/utilities/UserEmailer.java
@@ -2,11 +2,26 @@ package ly.unnecessary.backend.utilities;
 
 import java.util.UUID;
 
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.email.EmailBuilder;
+
 public class UserEmailer {
+    private Mailer mailer;
+    private Email templateEmail;
+
+    public UserEmailer(Mailer mailer, Email templateEmail) {
+        this.mailer = mailer;
+        this.templateEmail = templateEmail;
+    }
+
     public String requestSignUpConfirmation(String email) {
         var token = UUID.randomUUID();
 
-        System.out.printf("Sending confirmation mail to %s with token %s\n", email, token.toString());
+        var emailToSend = EmailBuilder.copying(this.templateEmail).to("", email)
+                .withPlainText(String.format("%s%s", this.templateEmail.getPlainText(), token.toString())).buildEmail();
+
+        this.mailer.sendMail(emailToSend);
 
         return token.toString();
     }
