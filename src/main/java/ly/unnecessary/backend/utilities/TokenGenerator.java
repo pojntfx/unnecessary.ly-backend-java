1d8e2662b555b45f560f4a4d1a5b68a2fa31d8f1
Felix Pojtinger <felix@pojtinger.com>
2020-07-07 01:15:36 +0200

    docs: Start adding JavaDocs

--- src/main/java/ly/unnecessary/backend/utilities/TokenGenerator.java
+++ src/main/java/ly/unnecessary/backend/utilities/TokenGenerator.java
@@ -3,6 +3,11 @@ package ly.unnecessary.backend.utilities;
 import java.util.UUID;
 
 public class TokenGenerator {
+    /**
+     * Generate a random token
+     * 
+     * @return String
+     */
     public String generateToken() {
         return UUID.randomUUID().toString();
     }
