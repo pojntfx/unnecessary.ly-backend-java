7e459b66c37bc2e0f94adc6d58fbbf4ae42e02c5
Felix Pojtinger <felix@pojtinger.com>
2020-07-07 00:22:33 +0200

    refactor: Decompose headers

--- src/main/java/ly/unnecessary/backend/interceptors/UserInterceptor.java
+++ src/main/java/ly/unnecessary/backend/interceptors/UserInterceptor.java
@@ -11,11 +11,14 @@ import io.grpc.ServerInterceptor;
 import static io.grpc.Metadata.ASCII_STRING_MARSHALLER;
 
 public class UserInterceptor implements ServerInterceptor {
-    public static Context.Key<String> USER_EMAIL = Context.key("x-uly-email");
-    public static Context.Key<String> USER_PASSWORD = Context.key("x-uly-password");
+    private static String EMAIL_HEADER = "x-uly-email";
+    private static String PASSWORD_HEADER = "x-uly-password";
 
-    public static Metadata.Key<String> USER_EMAIL_KEY = Metadata.Key.of("x-uly-email", ASCII_STRING_MARSHALLER);
-    public static Metadata.Key<String> USER_PASSWORD_KEY = Metadata.Key.of("x-uly-password", ASCII_STRING_MARSHALLER);
+    public static Context.Key<String> USER_EMAIL = Context.key(EMAIL_HEADER);
+    public static Context.Key<String> USER_PASSWORD = Context.key(PASSWORD_HEADER);
+
+    public static Metadata.Key<String> USER_EMAIL_KEY = Metadata.Key.of(EMAIL_HEADER, ASCII_STRING_MARSHALLER);
+    public static Metadata.Key<String> USER_PASSWORD_KEY = Metadata.Key.of(PASSWORD_HEADER, ASCII_STRING_MARSHALLER);
 
     @Override
     public <ReqT, RespT> Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers,
